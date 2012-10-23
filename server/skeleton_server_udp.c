d83d55d7f7dfa19e0f49f2c79a0f4cbb631676cd
Thomas Tsang <thomaswtsang@gmail.com>
2012-10-22 17:47:38 -0700

    working on udp client

--- server/skeleton_server_udp.c
+++ server/skeleton_server_udp.c
@@ -16,9 +16,12 @@ void die (char *s){
 }
 
 int main (void){
-	struct sockadder_in si_me, si_other;
+	printf ("...booting up...\n");
+
+	struct sockaddr_in si_me, si_other;
 	int sockfd, i, ret, slen;
 	char buf[MAX];
+	char ip_addr[INET_ADDRSTRLEN];
 
 	slen = sizeof(si_other);
 
@@ -29,11 +32,13 @@ int main (void){
 
 	memset((char *) &si_me, 0, sizeof(si_me));
 	si_me.sin_family = AF_INET;
- 	si_me.sin_port = htons(*port);
+ 	si_me.sin_port = htons(PORT);
  	si_me.sin_addr.s_addr = htonl(INADDR_ANY);	
- 	if (bind (httpd, (struct sockaddr*) &si_me, sizeof(si_me)) < 0){
+ 	if (bind (sockfd, (struct sockaddr*) &si_me, sizeof(si_me)) < 0){
  		die ("bind()");
  	}
+
+ 	printf ("...waiting for clients...\n");
 
  	//run forever
  	for(;;){
@@ -41,10 +46,20 @@ int main (void){
  		if (ret < 0){
  			die ("recvfrom()");
  		}
- 		printf ("==Received packet from %s:%d==\n%s\n", 
- 				inet_ntop (si_other.sin_addr), ntosh (si_other.sin_port), buf);
 
- 		sendto (sockfd, buf, strlen (buf)+1, 0, (struct sockadder*) &si_other, slen);
+ 		// store IP address in ip_addr
+ 		inet_ntop (AF_INET, &(si_me.sin_addr), ip_addr, INET_ADDRSTRLEN);
+
+ 		printf ("==Received packet from %s:%d==\n%s\n", 
+ 				ip_addr,
+ 				ntohs (si_other.sin_port), 
+ 				buf
+ 		);
+
+ 		ret = sendto (sockfd, buf, strlen (buf)+1, 0, (struct sockadder*) &si_other, slen);
+ 		if (ret < 0){
+ 			die ("sendto()");
+ 		}
  	}
 
  	close (sockfd);
