1b57d5daa4c1fed18d18ea863bf44012116f0eb8
Thomas Tsang <thomaswtsang@gmail.com>
2012-10-23 17:13:09 -0700

    refactor: variable names and additional layer for error checking

--- lib/207layer.h
+++ lib/207layer.h
@@ -3,4 +3,16 @@
 
 //struct union definition of packet header
 
+void die (char *s){
+	perror(s);
+	exit(1);
+}
+
+void check_for_error(int ret, char* s){
+	if (ret < 0){
+		die (s);
+	}
+}
+
+
 #endif
