6e25fb340ace3f577e68cb7ec5484e84a907e7cf
Thomas Tsang <thomaswtsang@gmail.com>
2012-10-25 01:11:15 -0700

    added self IP detection. makes for easier testing

--- server/skeleton_server_udp.c
+++ server/skeleton_server_udp.c
@@ -8,6 +8,11 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+
 #include "../lib/207layer.h"
 
 #define MAX		512
@@ -20,6 +25,85 @@ struct t_data{
 	int slen;
 	int ret;
 };
+
+void get_self_ip (char* addressBuffer);
+void* rw (void * data);
+
+
+int main (void){
+	printf ("...booting up...\n");
+
+	struct sockaddr_in s_server, s_client;
+	int sockfd, ret, slen, t_good;
+	char buf[MAX];
+	struct t_data rw_data;
+	pthread_t t_id;
+	char self_addr[INET_ADDRSTRLEN];
+
+	slen = sizeof(s_client);
+
+	sockfd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sockfd < 0) {
+		die ("socket()");
+	}
+
+	memset((char *) &s_server, 0, sizeof(s_server));
+	s_server.sin_family = AF_INET;
+ 	s_server.sin_port = htons(PORT);
+ 	s_server.sin_addr.s_addr = htonl(INADDR_ANY);	
+ 	if (bind (sockfd, (struct sockaddr*) &s_server, sizeof(s_server)) < 0){
+ 		die ("bind()");
+ 	}
+
+ 	get_self_ip (self_addr);
+
+ 	printf ("== %s : %i ==\n", self_addr, PORT);
+ 	printf ("...waiting for clients...\n");
+	
+ 	//run forever
+ 	for(;;){
+
+ 		ret = recvfrom (sockfd, buf, MAX, 0, (struct sockaddr*)&s_client, &slen);
+ 		check_for_error (ret, "recvfrom()");
+
+		rw_data.fd = sockfd;
+		rw_data.buffer = buf;
+		rw_data.client = &s_client;
+		rw_data.slen = slen;
+		rw_data.ret = ret;
+
+		ret = pthread_create(&t_id, NULL, rw, (void*)&rw_data);
+		check_for_error(ret, "pthread_create()");
+ 	}
+
+ 	close (sockfd);
+	pthread_exit(NULL);
+ 	return 0;
+}
+
+void get_self_ip (char* addressBuffer){
+	struct ifaddrs * ifAddrStruct = NULL;
+    struct ifaddrs * ifa  		  = NULL;
+    void * tmpAddrPtr 			  = NULL;
+    //char addressBuffer[INET_ADDRSTRLEN];
+
+    getifaddrs(&ifAddrStruct);
+
+    for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa ->ifa_addr->sa_family==AF_INET) { 
+        	// only IPv4 address
+            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+
+            if (strcmp(ifa->ifa_name, "en0") == 0 
+            	|| strcmp(ifa->ifa_name, "eth0") == 0){
+                inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);    
+            	//printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);     
+            }  
+        } 
+    }
+    if (ifAddrStruct!=NULL) 
+    	freeifaddrs(ifAddrStruct);
+}
 
 void *rw(void * data){
 	
@@ -42,50 +126,4 @@ void *rw(void * data){
 	
 	ret = sendto (sockfd, buf, strlen (buf)+1, 0, (struct sockaddr*)s_client, slen);
 	check_for_error (ret, "sendto()");
-}
-
-int main (void){
-	printf ("...booting up...\n");
-
-	struct sockaddr_in s_server, s_client;
-	int sockfd, ret, slen, t_good;
-	char buf[MAX];
-	struct t_data rw_data;
-	pthread_t t_id;
-
-	slen = sizeof(s_client);
-
-	sockfd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sockfd < 0) {
-		die ("socket()");
-	}
-
-	memset((char *) &s_server, 0, sizeof(s_server));
-	s_server.sin_family = AF_INET;
- 	s_server.sin_port = htons(PORT);
- 	s_server.sin_addr.s_addr = htonl(INADDR_ANY);	
- 	if (bind (sockfd, (struct sockaddr*) &s_server, sizeof(s_server)) < 0){
- 		die ("bind()");
- 	}
-
- 	printf ("...waiting for clients...\n");
-	
- 	//run forever
- 	for(;;){
-
- 		ret = recvfrom (sockfd, buf, MAX, 0, (struct sockaddr*)&s_client, &slen);
- 		check_for_error (ret, "recvfrom()");
-
-		rw_data.fd = sockfd;
-		rw_data.buffer = buf;
-		rw_data.client = &s_client;
-		rw_data.slen = slen;
-		rw_data.ret = ret;
-
-		t_good = pthread_create(&t_id, NULL, rw, (void*)&rw_data);
- 	}
-
- 	close (sockfd);
-	pthread_exit(NULL);
- 	return 0;
 }
