048b8d4b0f31014a251272e07b28574bcb9b251d
ShreshthaB <shreshthabandi@gmail.com>
2012-11-27 10:16:01 -0800

    Update lib/connect207.h
    Shreshtha: Updated 

--- lib/connect207.h
+++ lib/connect207.h
@@ -5,7 +5,8 @@
 #define TCP207_SUCCESS 0	/*generic success*/
 #define TCP207_ERROR 2	/*generic error*/
 #define TCP207_ERROR_NULL_POINTER 3 /*NULL pointer not expected error*/
-#define TCP207_CONNECT_WRONG_3WAY_RESPONSE /*Not a valid respnse in 3way handshake*/
+#define TCP207_CONNECT_ERROR_WRONG_3WAY_RESPONSE 4 /*Not a valid respnse in 3way handshake*/
+#define TCP207_ERROR_TCPSTATE_MISBEHAVE 5 /*TCP state machine misbehave*/
 
 #define CONNECT207_SYN 0 	/*SYN state*/
 #define CONNECT207_SYN_ACK 1 	/*SYN| ACK state*/
@@ -14,20 +15,6 @@
 
 
 
-struct myTcpBlock
-{
-	int sockfd;
-	struct sockaddr_in *pSocket_info;
-	struct packet_header *pTcpH;
-	struct packet_header *pSentTcpH;
-	struct sequence *pSeq;
-	int tcp_current_state;
-	int tcp_prev_state;
-
-};
-
-struct myTcpBlock gTcp_Block[MAX_TCB_SIZE];
-
 void connect207_print_tcp_header(int tcp_block_index_in);
-int tcp_header_extract_from_recv_packet(int tcp_block_index_in, char * pBuffer_in );
-#endif
+
+int connect207(int tcp_block_index_in, int clientOrServer_in  );
